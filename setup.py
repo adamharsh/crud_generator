07ded54a21826adb102cb07e8928c3ec6445609a
Harsh Goyal <harshgoyal.india@gmail.com>
2020-04-22 11:08:39 +0530

    first

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='crud_generator',
+    version='1.0.1',
+    description='This will generate a crud operations (crud.py) for your Database tables.',
+    py_modules=["crud_generator"],
+)
