1a880443b7c0ecd10e5d1d7cd324531c1a70855d
Harsh Goyal <harshgoyal.india@gmail.com>
2020-04-22 11:15:58 +0530

    crud

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
-
+with open("README.md","r") as fh:
+    long_description = fh.read()
 setup(
     name='crud_generator',
     version='1.0.1',
     description='This will generate a crud operations (crud.py) for your Database tables.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     py_modules=["crud_generator"],
 )
